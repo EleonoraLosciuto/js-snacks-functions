76b0b870102a77328e82b1b58ee053926a0c757f
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-25 22:22:00 +0200

    exercise 02 iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const initials = (nameList) => {
+    let arrInitials = [];
+    for (let i = 0; i < nameList.length; i++) {
+    arrInitials.push(nameList[i][0]);
+    };
+    return arrInitials;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
