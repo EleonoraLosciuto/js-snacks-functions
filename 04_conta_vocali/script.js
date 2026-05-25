71ffbe393c40bb74d24f92aeef0e3c7bb9e55a22
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-25 23:27:42 +0200

    exercise 04 conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,17 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+const vowelNum = (string) => {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i].toLowerCase() === "a" || string[i].toLowerCase() === "e" || string[i].toLowerCase() === "i" || string[i].toLowerCase() === "o" || string[i].toLowerCase() === "u")
+        count += 1;
+    };
+    return count;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelNum(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
